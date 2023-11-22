dcaa10a0602a44a1d55c46c887b2e9c92ed6a50c
wa1ead <waliddraa2@gmail.com>
2023-11-22 11:47:12 +0100

    current state callback

--- steps/src/App.js
+++ steps/src/App.js
@@ -11,15 +11,15 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
     <div>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((open) => !open)}>
         &times;
       </button>
       {isOpen && (
