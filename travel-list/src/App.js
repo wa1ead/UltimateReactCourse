8e4e3fdee9d74fd5ced85bfb456c6939cc631d0f
wa1ead <waliddraa2@gmail.com>
2023-11-29 14:44:49 +0100

    clear list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -19,6 +19,11 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm("Are you sure?");
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -27,6 +32,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -75,7 +81,7 @@ function Form({ onAddItems }) {
     </form>
   );
 }
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState("input");
   let sortedItems;
   if (sortBy === "input") sortedItems = items;
@@ -95,6 +101,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
             item={item}
             onDeleteItem={onDeleteItem}
             onToggleItem={onToggleItem}
+            onClearList={onClearList}
             key={item.id}
           />
         ))}
@@ -105,6 +112,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearList}>Clear List</button>
       </div>
     </div>
   );
