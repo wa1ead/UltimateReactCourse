4214c7282aa6c7b87042c2e61fb0756b1d1c9030
wa1ead <waliddraa2@gmail.com>
2023-12-01 15:41:19 +0100

    message children prop

--- steps/src/App.js
+++ steps/src/App.js
@@ -37,9 +37,7 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <Message step={step}>{messages[step - 1]}</Message>
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
               <span>👈🏼</span>Previous
@@ -50,6 +48,15 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function Message({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step: {step}</h3>
+      {children}
     </div>
   );
 }
