e192bf1ba30551c060a1d241d4f19c5c440b2a3c
wa1ead <waliddraa2@gmail.com>
2023-12-01 15:54:53 +0100

    accordion component v2

--- AccordionComponent/src/App.js
+++ AccordionComponent/src/App.js
@@ -4,46 +4,60 @@ import "./styles.css";
 const faqs = [
   {
     title: "Where are these chairs assembled?",
-    text:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
   {
     title: "How long do I have to return my chair?",
-    text:
-      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
   },
   {
     title: "Do you ship to countries outside the EU?",
-    text:
-      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-  }
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
 ];
 
 export default function App() {
   return (
-    <div className="accordion">
-      <Accordion data={faqs}/>
+    <div>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion({data}) {
-  return <div>
-    {data.map((el, i) => <AccordionItem title={el.title} text={el.text} num={i} key={i} />)}
-  </div>;
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
+      ))}
+    </div>
+  );
 }
 
-function AccordionItem({num, title, text}){
-  const [isOpen, setIsOpen] = useState(false);
-  function handleToggle(){
-    setIsOpen(isOpen => !isOpen)
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
   }
 
   return (
-  <div className={isOpen ? "open" : "item"} onClick={handleToggle}>
-    <p className="number">{num < 9 ? `0${num+1}` : num+1}</p>
-    <p className="title">{title}</p>
-    <p className="icon">{isOpen ? "-" : "+"}</p>
-    {isOpen && <div className="content-box">{text}</div>}
-  </div>
-)}
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{children}</div>}
+    </div>
+  );
+}
