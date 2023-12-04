11ad991d4555ad313311308dded610df3faacaaa
wa1ead <waliddraa2@gmail.com>
2023-12-04 15:04:07 +0100

    starter

---
+++ tipCalculator/src/App.js
@@ -0,0 +1,60 @@
+import './styles.css';
+import {useState} from 'react';
+
+export default function App(){
+  return(
+  <div>
+    <TipCalculator/>
+  </div>
+  );
+}
+
+function TipCalculator(){
+  const [bill, setBill] = useState('');
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100)
+  return(
+    <div>
+      <BillInput bill={bill} onSetBill={setBill}/>
+      <PercentageSelector percentage={percentage1} onSelect={setPercentage1}>How did you like the service?</PercentageSelector>
+      <PercentageSelector percentage={percentage2} onSelect={setPercentage2}>How did your friend like the service?</PercentageSelector>
+      <Output bill={bill} tip={tip}/>
+      <Reset />
+    </div>
+  );
+}
+
+function BillInput({bill, onSetBill}){
+  return (
+  <div>
+    <label>How much was the bill?</label>
+    <input type="text" placeholder="Bill Value" value={bill} onChange={(e) => onSetBill(Number(e.target.value))}/>
+    </div>
+);}
+
+function PercentageSelector({children, percentage, onSelect}){
+  return(
+    <div>
+      <label>{children}</label>
+      <select value={percentage} onChange={e=>onSelect(Number(e.target.value))}>
+        <option value="0">Disatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output({bill, tip}){
+return(
+  <div>
+    <h3>You pay {bill + tip} + (${bill} + ${tip})</h3>
+  </div>
+);
+}
+
+function Reset(){
+  return<button>Reset</button>
+}
