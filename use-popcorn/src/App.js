1ae5c8afa90f7b7a0d9f8ba3ff79d6160b9b8d52
wa1ead <waliddraa2@gmail.com>
2023-12-20 17:14:02 +0100

    async function

--- use-popcorn/src/App.js
+++ use-popcorn/src/App.js
@@ -52,12 +52,16 @@ const KEY = "dfdf180e";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const query = "inception";
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=inception
-  `)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}
+  `);
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
   }, []);
 
   return (
