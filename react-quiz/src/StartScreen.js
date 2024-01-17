39898577813209d7ceaeb32a4406dc5bfd2ea02e
wa1ead <waliddraa2@gmail.com>
2024-01-17 11:31:56 +0100

    loading, error & ready status

---
+++ react-quiz/src/StartScreen.js
@@ -0,0 +1,10 @@
+function StartScreen({ numQuestions }) {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>{numQuestions} question to test your React mastery</h3>
+      <button className="btn btn-ui">Let's Start</button>
+    </div>
+  );
+}
+export default StartScreen;
