b21de472f91c65becbc368e55f81780f05153c5f
wa1ead <waliddraa2@gmail.com>
2024-01-17 11:42:55 +0100

    starting the quiz

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -17,6 +18,10 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+
+    case "start":
+      return { ...state, status: "active" };
+
     default:
       throw new Error("Action Undefined");
   }
@@ -40,7 +45,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
---
+++ react-quiz/src/Question.js
@@ -0,0 +1,8 @@
+function Question() {
+  return (
+    <div>
+      <h2>Question</h2>
+    </div>
+  );
+}
+export default Question;
