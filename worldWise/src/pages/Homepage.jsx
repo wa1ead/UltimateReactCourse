c2cc196a75070ac391ce4ef02d3c69ef5a9e2ffa
wa1ead <waliddraa2@gmail.com>
2024-01-22 17:04:49 +0100

    linking between routes

--- worldWise/src/pages/Homepage.jsx
+++ worldWise/src/pages/Homepage.jsx
@@ -1,4 +1,11 @@
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-  return <div>WorldWise</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+    </div>
+  );
 }
 export default Homepage;
