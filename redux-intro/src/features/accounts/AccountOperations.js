c3c639b21eb3c068561e4074a3c1f0081eec8f89
wa1ead <waliddraa2@gmail.com>
2024-02-12 18:41:51 +0100

    API call: React thunk

--- redux-intro/src/features/accounts/AccountOperations.js
+++ redux-intro/src/features/accounts/AccountOperations.js
@@ -19,12 +19,14 @@ function AccountOperations() {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
     balance,
+    isLoading,
   } = useSelector((store) => store.account);
 
   function handleDeposit() {
     if (!depositAmount) return;
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
+    setCurrency("");
   }
 
   function handleWithdrawal() {
@@ -64,7 +66,9 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
 import customerReducer from "../src/features/customers/CustomerSlice";
 import accountReducer from "../src/features/accounts/AccountSlice";
 
@@ -7,6 +8,6 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
