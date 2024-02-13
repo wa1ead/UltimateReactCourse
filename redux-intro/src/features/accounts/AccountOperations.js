357e456a15259c5c60edcab5bf402cd28912c3e2
wa1ead <waliddraa2@gmail.com>
2024-02-13 12:38:11 +0100

    store: Redux toolkit

--- redux-intro/src/features/accounts/AccountOperations.js
+++ redux-intro/src/features/accounts/AccountOperations.js
@@ -26,7 +26,7 @@ function AccountOperations() {
     if (!depositAmount) return;
     dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
-    setCurrency("");
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,13 +1,15 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
+// import { applyMiddleware, combineReducers, createStore } from "redux";
+// import { thunk } from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "../src/features/customers/CustomerSlice";
 import accountReducer from "../src/features/accounts/AccountSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
