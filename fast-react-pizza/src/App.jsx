2313b318c8548bf62f6c3cde33f0b66c4c0da3e3
wa1ead <waliddraa2@gmail.com>
2024-02-14 13:25:16 +0100

    building the app layout

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,21 +1,27 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import AppLayout from "./ui/AppLayout";
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 
-const routes = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/:orderId", element: <Order /> },
-  { path: "/oreder/new", element: <CreateOrder /> },
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/:orderId", element: <Order /> },
+      { path: "/oreder/new", element: <CreateOrder /> },
+    ],
+  },
 ]);
 
 function App() {
-  return <RouterProvider router={routes} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
