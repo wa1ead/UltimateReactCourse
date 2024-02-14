41ff0acef70fc2067f4ee3908600d50a6e16a3b5
wa1ead <waliddraa2@gmail.com>
2024-02-14 13:04:18 +0100

    implementing routes: NEW WAY

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,11 +1,21 @@
-function App() {
-  const x = 22;
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-  return (
-    <div>
-      <h1>Hello L3alam</h1>
-    </div>
-  );
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+
+const routes = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/:orderId", element: <Order /> },
+  { path: "/oreder/new", element: <CreateOrder /> },
+]);
+
+function App() {
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
