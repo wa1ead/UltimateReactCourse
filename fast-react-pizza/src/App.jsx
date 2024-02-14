5bbdda5a50316e4851b1e2ff95bf995e859d835f
wa1ead <waliddraa2@gmail.com>
2024-02-14 15:58:23 +0100

    handling errors: ErrorElement

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -6,13 +6,20 @@ import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        errorElement: <Error />,
+        loader: menuLoader,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/:orderId", element: <Order /> },
       { path: "/oreder/new", element: <CreateOrder /> },
