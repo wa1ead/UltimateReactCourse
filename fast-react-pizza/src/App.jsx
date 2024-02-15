eca8ffd95a4cf43ce5b5e5b48cf4e676355f0dcf
wa1ead <waliddraa2@gmail.com>
2024-02-15 13:16:03 +0100

    creating actions: React Router

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -5,7 +5,9 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Error from "./ui/Error";
 
 const router = createBrowserRouter([
@@ -27,7 +29,11 @@ const router = createBrowserRouter([
         errorElement: <Error />,
         loader: orderLoader,
       },
-      { path: "/oreder/new", element: <CreateOrder /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
     ],
   },
 ]);
