6c335b7b5d7ad125567daac197a0e5a9a3f7c443
wa1ead <waliddraa2@gmail.com>
2024-02-17 12:50:12 +0100

    the box model

--- fast-react-pizza/src/ui/header.jsx
+++ fast-react-pizza/src/ui/header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-2">
       <Link to="/" className="uppercase tracking-widest">
         Fast React Pizza Co.
       </Link>
