19c41dbdb41b3515ddc95510fd18a6fb885194ae
wa1ead <waliddraa2@gmail.com>
2024-02-19 16:35:52 +0100

    styling the menu

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "inline-block rounded-full bg-yellow-400 px-4 py-3 text-sm font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-200 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed";
+function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block rounded-full bg-yellow-400 font-semibold uppercase text-stone-800 transition-colors duration-200 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " text-sm tracking-wide px-4 py-3 md:px-6 md:py-4",
+    small: base + " text-xs tracking-small px-2 py-1 md:px-4 md:py-2",
+  };
 
   if (to)
     return (
-      <Link className={className} to={to}>
+      <Link className={styles[type]} to={to}>
         {children}
       </Link>
     );
 
   return (
-    <button className={className} disabled={disabled}>
+    <button className={styles[type]} disabled={disabled}>
       {children}
     </button>
   );
