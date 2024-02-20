7b0bb0ea8a10155f4d40d84da8d1797837750b79
wa1ead <waliddraa2@gmail.com>
2024-02-20 12:19:30 +0100

    styling the cart

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " text-sm tracking-wide px-4 py-3 md:px-6 md:py-4",
     small: base + " text-xs tracking-small px-2 py-1 md:px-4 md:py-2",
+    secondary:
+      "inline-block text-sm tracking-wide px-4 py-3 md:px-6 md:py-4 rounded-full font-semibold uppercase border-2 text-stone-400 border-stone-300 transition-colors duration-200 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus-text-stone-800 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed",
   };
 
   if (to)
