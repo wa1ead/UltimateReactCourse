2ca07520fab5e2efa24382d83fadf9a1c6e5344c
wa1ead <waliddraa2@gmail.com>
2024-02-21 13:16:44 +0100

    reading & updating user state

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -35,6 +36,7 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+  const username = useSelector((state) => state.user.username);
 
   const formErrors = useActionData();
 
@@ -48,7 +50,13 @@ function CreateOrder() {
       <Form method="POST" action="/order/new">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input " type="text" name="customer" required />
+          <input
+            className="input"
+            type="text"
+            defaultValue={username}
+            name="customer"
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- fast-react-pizza/src/ui/Home.jsx
+++ fast-react-pizza/src/ui/Home.jsx
@@ -1,7 +1,9 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
 import Button from "./Button";
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
   return (
     <div className="my-8 text-center sm:my-10">
       <h1 className=" mb-4 text-base font-semibold text-stone-900 sm:mb-8 sm:text-xl md:text-3xl">
@@ -11,7 +13,13 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {username === "" ? (
+        <CreateUser />
+      ) : (
+        <Button type="primary" to="/menu">
+          Go to menu
+        </Button>
+      )}
     </div>
   );
 }
