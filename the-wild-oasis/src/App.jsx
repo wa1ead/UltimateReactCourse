3c032ea485a1e14005ddca83e45851a60ccead3f
wa1ead <waliddraa2@gmail.com>
2024-02-28 13:24:17 +0100

    styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,4 +1,30 @@
+import styled from "styled-components";
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: green;
+`;
+const Button = styled.button`
+  font-size: 20px;
+  padding: 16px 20px;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: yellow;
+  color: blue;
+  margin: 8px;
+`;
+const StyledApp = styled.div`
+  background-color: red;
+  padding: 8px;
+`;
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <StyledApp>
+      <H1>Hello World</H1>
+      <Button>Check In</Button>
+    </StyledApp>
+  );
 }
 export default App;
