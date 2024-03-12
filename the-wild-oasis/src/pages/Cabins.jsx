bc029250e56c6248d28116f2c8b476cd52f02032
wa1ead <waliddraa2@gmail.com>
2024-03-12 14:19:07 +0000

    React Hook Form

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
 
 function Cabins() {
   // useEffect(function () {
   //   getCabins().then((data) => console.log(data));
   // }, []);
+
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <>
@@ -15,6 +20,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
