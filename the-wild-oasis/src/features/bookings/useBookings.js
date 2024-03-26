100f9cafd859d153ef7461fdfa20892d1f839008
wa1ead <waliddraa2@gmail.com>
2024-03-26 14:16:44 +0000

    API-Side filtering: Bookings

--- the-wild-oasis/src/features/bookings/useBookings.js
+++ the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,11 +1,25 @@
 /* eslint-disable no-unused-vars */
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+
 import { getBookings } from "../../services/apiBookings";
 
 export function useBookings() {
-  const { data: bookings, isLoading } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+  const [searchParams] = useSearchParams();
+
+  //FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  const {
+    data: bookings,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
-  return { bookings, isLoading };
+  return { bookings, isLoading, error };
 }
